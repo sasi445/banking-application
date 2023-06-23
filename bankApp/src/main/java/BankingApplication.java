398782352f6821033f9d7fc145f405187c2b5ed4
sasi445 <sasi@woodenpanda.ca>
2023-06-23 16:25:34 -0400

    initial version

---
+++ bankApp/src/main/java/BankingApplication.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class BankingApplication {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+        displayOptions();
+    }
+
+    private static void displayWelcomeMessage() {
+        System.out.println("Welcome to the Bank Application System!");
+    }
+
+    private static void displayOptions() {
+        boolean isLoggedIn = false;
+
+        while (true) {
+            System.out.println("\nPlease select an option:");
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("0. Exit\n");
+
+            int choice = getIntInput();
+
+            switch (choice) {
+                case 1 -> System.out.println("Registration option selected.");
+
+                // Implement registration logic
+                case 2 -> {
+                    System.out.println("Login option selected.");
+                    isLoggedIn = login();
+                }
+                case 0 -> {
+                    System.out.println("Exiting the Bank Application System...");
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+
+            if (isLoggedIn) {
+                displayLoggedInOptions();
+                break;
+            }
+        }
+    }
+
+    private static boolean login() {
+        System.out.println("Login successful.");
+        // Implement login logic and return true if login is successful, otherwise false
+        return true;
+    }
+
+    private static void displayLoggedInOptions() {
+        while (true) {
+            System.out.println("\nPlease select an option:");
+            System.out.println("1. Balance Inquiry");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdrawal");
+            System.out.println("4. Fund Transfer");
+            System.out.println("0. Exit\n");
+
+            int choice = getIntInput();
+
+            switch (choice) {
+                case 1 -> System.out.println("Balance Inquiry option selected.");
+
+                // Implement balance inquiry logic
+                case 2 -> System.out.println("Deposit option selected.");
+
+                // Implement deposit logic
+                case 3 -> System.out.println("Withdrawal option selected.");
+
+                // Implement withdrawal logic
+                case 4 -> System.out.println("Fund Transfer option selected.");
+
+                // Implement fund transfer logic
+                case 0 -> {
+                    System.out.println("Exiting the Bank Application System...");
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+
+    private static int getIntInput() {
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
